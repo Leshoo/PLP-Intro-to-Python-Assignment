4829324c0e0a4b098870992164245aedb1642b2a
Godfrey Leshoo <92622095+Leshoo@users.noreply.github.com>
2024-11-06 13:16:53 +0300

    Create user_input.py

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("What is your name? ")
+age = input("How old are you? ")
+location = input("Where are you located? ")
+
+print(f"\nHello, {name}! You are {age} years old and currently in {location}. Nice to meet you!")
